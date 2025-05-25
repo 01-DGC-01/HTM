4f159cbfdeb8fde371a5d92ab461169343f56dec
01-DGC-01 <DGC_OUT@outlook.com>
2025-05-25 19:54:54 +0800

    Add files via upload

---
+++ Template.cpp
@@ -0,0 +1,20 @@
+//2025-5-25
+//ID:1
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+#include <cstdio>
+#include <string>
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    
+
+    return 0;
+}
